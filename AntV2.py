5c5141d6f6cf7e5384cd6ec8d2bbaff47c461790
Guilherme <guilhermebianeck@gmail.com>
2022-09-14 11:17:38 -0300

    Create AntV2.py
    IA V.0

---
+++ AntV2.py
@@ -0,0 +1,38 @@
+
+import numpy as np
+
+
+
+def generate_matrix(size):
+	matrix=np.zeros(size)
+	return matrix
+
+
+def verify_pos(matrix,pos)
+	return matrix[pos]
+
+def random_object(amount_object, object_type):
+
+
+
+
+'''
+Montar Sistema para Simular Agrupamento de Itens:
+
+Definir uma Matriz (Tamanho)
+
+Definir Quantidade de Formigas 
+
+Verificar se é inferior ao tamanho da matriz
+
+Definir Quantidade de Objetos
+
+Verificar se é inferior ao Tamanho da matriz - Quantidade de Formigas
+
+Botar Formigas na Matriz (Estados)
+
+Verificar se Campo não esta ocupado
+
+Botar Objetos na Matriz (Estados)
+
+Verificar se Campo não esta ocupado'''
