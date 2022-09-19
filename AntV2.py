36c5147e2ee0f8b4e665b8a8297a1ccae975ce37
Guilherme <guilhermebianeck@gmail.com>
2022-09-18 22:47:34 -0300

    AI Infura Update

--- AntV2.py
+++ AntV2.py
@@ -1,19 +1,54 @@
 
 import numpy as np
+from web3 import Web3, HTTPProvider
+import random
 
+connection = Web3(HTTPProvider('https://mainnet.infura.io/v3/YOUR_TOKEN'))
 
 
 def generate_matrix(size):
 	matrix=np.zeros(size)
 	return matrix
 
-
-def verify_pos(matrix,pos)
+def verify_pos(matrix,pos):
 	return matrix[pos]
 
 def random_object(amount_object, object_type):
+	return amount_object
+
+def 
 
 
+def main():
+
+	out = 0
+
+	size = int(input('Enter Matrix Size : '))
+	print ('Size ', size)
+	block = connection.eth.get_block('latest')
+	seed = (int(block['hash'].hex(),16))
+	seed = random.seed(seed)
+
+	print(random.random())
+	matrix = generate_matrix(size)
+
+	n_ants = input('Number of Ants: ')
+
+	#if n_ants < size ** 2:
+	#	out = 1
+
+	#while (out==0)
+
+	#	print ('Too Much Ants, Try a lower Amount')
+
+
+
+	#n_dead =
+
+	#Gerar 
+
+if __name__ == '__main__':
+    main()
 
 
 '''
